552172f3e78e64ded29923d9416cd8dabd236b22
Milena Neves <milena.freire2013@gmail.com>
2023-07-03 14:52:13 -0300

    operações
    aritméticas, lógicas, push, pop e operadores relacionais

---
+++ Command.java
@@ -0,0 +1,18 @@
+
+public class Command {
+
+    public enum Type {
+        ADD, 
+        SUB,
+        NEG,
+        EQ,
+        GT,
+        LT,
+        AND,
+        OR,
+        NOT,
+        PUSH,
+        POP,
+        ;
+    }
+}
